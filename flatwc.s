2597eb8d1af60f27737d9278f639098cf5b42807
nick-casa <nc16@princeton.edu>
2021-04-22 17:59:25 -0400

    Working on assembly

--- flatwc.s
+++ flatwc.s
@@ -5,6 +5,15 @@
 
         .equ    TRUE, 1
         .equ    FALSE, 0
+//----------------------------------------------------------------------
+
+        .section .rodata
+
+newLine:
+    .string '\n'
+
+promptStr:
+    .string "%7ld %7ld %7ld\n"
 
 //----------------------------------------------------------------------
         .section .data
@@ -16,7 +25,7 @@ lWordCount:
 lCharCount:
         .quad   0
 iInWord:
-        .byte   FALSE
+        .word   FALSE
 
 //----------------------------------------------------------------------
         .section .bss
@@ -44,9 +53,10 @@ main:
     str     x30, [sp]
 inputLoop:
     // if((iChar = getchar()) == EOF) goto endInputLoop //
-    bl      getchar
+    bl      getchar  // w0 stores the value of getchar
     adr     x1, iChar
-    str     x0, [x1]
+    str     w0, [x1] // iChar = getchar()
+    cmp     w0, -1 // iChar == -1
     beq     endInputLoop
 
     // lCharCount++
@@ -56,30 +66,86 @@ inputLoop:
     str     x1, [x0]
 
     // if (!isspace(iChar)) goto else1; // if 1
+    adr     x0, iChar
+    ldr     x0, [x0]
+    bl      isspace
+    cmp     w0, FALSE
+    beq     else1
 
     // if (!iInWord) goto endif1; // if 2
+    adr     x0, iInWord
+    ldr     x0, [x0]
+    cmp     x0, FALSE
+    beq     endif1
 
     // lWordCount++;
+    adr     x0, lWordCount
+    ldr     x1, [x0]
+    add     x1, x1, 1
+    str     x1, [x0]
+
     // iInWord = FALSE;
+    adr     x0, iInWord
+    mov     w1, FALSE
+    str     w1, [x0]
+
     // goto endif1;
+    b       endif1
 else1:
     // if (iInWord) goto endif1;
+    adr     x0, iInWord
+    ldr     x0, [x0]
+    cmp     x0, TRUE
+    beq     endif1
+
     // iInWord = TRUE;
+    adr     x0, iInWord
+    mov     w1, TRUE
+    str     w1, [x0]
+
     // goto endif1;
+    b       endif1
 endif1:
     // if (iChar != '\n') goto inputLoop;
+    adr     x0, iChar
+    ldr     w0, [x0]
+    mov     w1, newLine
+    cmp     w0, w1
+    bne     endInputLoop
+
     // lLineCount++;
+    adr     x0, lLineCount
+    ldr     x1, [x0]
+    add     x1, x1, 1
+    str     x1, [x0]
+
     // goto inputLoop;
+    b       inputLoop
 endInputLoop:
     // if (!iInWord) goto endif;
+    adr     x0, iInWord
+    ldr     w0, [x0]
+    cmp     w0, FALSE
+    beq     endif
+
     // lWordCount++;
+    adr     x0, lWordCount
+    ldr     x1, [x0]
+    add     x1, x1, 1
+    str     x1, [x0]
+
 endif:
     // printf("%7ld %7ld %7ld\n", lLineCount, lWordCount, lCharCount);
-    // return 0;
+    adr     x0, promptStr
+    adr     x1, lLineCount
+    adr     x2, lWordCount
+    adr     x3, lCharCount
+    bl      printf
 
     // Epilog and return 0
     mov     w0, 0
     ldr     x30, [sp]
     add     sp, sp, MAIN_STACK_BYTECOUNT
+    // return 0;
     ret
     .size   main, (. - main)

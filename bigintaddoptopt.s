005c728fc10edb87b6db61221a04575047b49f96
nick-casa <nc16@princeton.edu>
2021-04-27 22:34:30 -0400

    Test files

--- bigintaddoptopt.s
+++ bigintaddoptopt.s
@@ -87,7 +87,10 @@ endif2:
 
 
 endBranch:
+    // ulSum = ulCarry
     mov     ULSUM, x4
+    // ulCarry = 0
+    adcs    x0, x0, xzr
 
     // ulSum += oAddend1->aulDigits[lIndex]
     add     x1, OADDEND1, 8            // gets to aulDigits

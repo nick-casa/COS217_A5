913a1d46500e0218afd4720ffd66393a60df8d02
nick-casa <nc16@princeton.edu>
2021-04-28 11:42:47 -0400

    SOMETHING VERY NOTICEABLE

--- bigintaddoptopt.s
+++ bigintaddoptopt.s
@@ -87,7 +87,7 @@ endBranch:
     // ulSum = ulCarry
     mov     ULSUM, x4
     // ulCarry = 0
-    // adcs    x0, x0, xzr
+    adcs    x0, x0, xzr
 
     // ulSum += oAddend1->aulDigits[lIndex]
     add     x1, OADDEND1, 8            // gets to aulDigits
